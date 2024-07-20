cfe5cd6318b85063f26d9141669a71edb2fb75a8
Divyasoniworld <divyasoniworld@gmail.com>
2024-07-21 00:24:04 +0530

    some changes

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -247,23 +247,7 @@ export default function chat() {
               </SheetContent>
             </Sheet>
           </nav>
-          {/* <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link
-                    href="#"
-                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                    prefetch={false}
-                  >
-                    <SettingsIcon className="h-5 w-5" />
-                    <span className="sr-only">Settings</span>
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent side="right">Settings</TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </nav> */}
+         
         </aside>
         <div className="main-chat-div flex-1 flex flex-col">
           <MainNav />
