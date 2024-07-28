8896a478d03b3621b116c06763fbbc615711cb7c
Divyasoniworld <divyasoniworld@gmail.com>
2024-07-28 12:07:59 +0530

    add meta info

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,32 +5,32 @@ import { useTheme } from "next-themes";
 // import { Toaster } from "@/components/ui/toaster"
 import { Toaster } from "react-hot-toast";
 export default function Document({ children }) {
- 
+
   return (
     <Html lang="en" suppressHydrationWarning >
-      <Head />
-      <body
-      className={cn(
-        " bg-background font-sans antialiased",
-      )}
-      >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme=""
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-          {/* <Toaster /> */}
-          {/* <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
-           */}
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+      <Head>
+        <meta property="og:title" content="Medify AI" />
+          <meta property="og:description" content="Know your meds, simplify your health" />
+            <meta property="og:image" content="https://ik.imagekit.io/medifyai/appImages/appImage.png" />
+            <link rel="icon" type="image/png" href="https://ik.imagekit.io/medifyai/appImages/appImage.png"></link>
+              <title>Medify AI</title>
+            </Head>
+            <body
+              className={cn(
+                " bg-background font-sans antialiased",
+              )}
+            >
+              <ThemeProvider
+                attribute="class"
+                defaultTheme=""
+                enableSystem
+                disableTransitionOnChange
+              >
+                {children}
+              </ThemeProvider>
+              <Main />
+              <NextScript />
+            </body>
+          </Html>
+          );
 }
